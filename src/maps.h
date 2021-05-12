cb8cc619ddf46e7b8b2ab2af504f0746b0f66bad
Sakits <sakits_tjm@sjtu.edu.cn>
2021-05-12 20:36:37 +0800

    hashmap completed

--- src/maps.h
+++ src/maps.h
@@ -18,9 +18,9 @@
 template<class Key, class Value, class Hash>
 class OuterUniqueUnorderMap {
 private:
-    char file[30];
-    BPlusTree bpt;
+    char file[30]; 
     std::fstream fio;
+    BPlusTree bpt;
 
 public:
     OuterUniqueUnorderMap(const char *_datafile) : bpt((std::string("bpt_")+_datafile).c_str()) {
@@ -93,37 +93,85 @@ public:
 //stub 唐嘉铭 todo
 template<class Key, class Value, class Hash>
 //Hash是一个模板类名，它实例化后的一个对象例为auto h = Hash<string>(), 这个对象重载了括号，比如可以h(1),然后返回一个size_t
-class InnerUniqueUnorderMap {
+class InnerUniqueUnorderMap 
+{
+private:
+    using ull = unsigned long long;
+    const int maxn, mod;
+    int tot = 0, size = 0;
+    int* last;
+    struct edge{Value val; ull too; int pre;} *e;
 public:
 
-    bool insert(const std::pair<Key, Value> &pair) {
-        return mapper.insert(pair).second;
+    // maxn 表示 insert 次数上限，_mod 表示哈希模数，最好是质数
+    InnerUniqueUnorderMap(int _maxn, int _mod) : maxn(_maxn), mod(_mod) 
+    {
+        last = new int [mod];
+        e = new edge [_maxn + 1];
     }
 
-    std::pair<Value, bool> erase(const Key &key) {
-        auto iter = mapper.find(key);
-        if (iter == mapper.end()) return {Value(), 0};
-        Value v = iter->second;
-        mapper.erase(iter);
-        return {v, 1};
+    ~InnerUniqueUnorderMap() {delete last; delete e;}
+
+    inline void add(int x, ull y, const Value &val)
+    {
+        e[++tot] = (edge){val, y, last[x]}; last[x] = tot;
+        size++;
     }
 
-    bool empty() const {
-        return mapper.empty();
+    bool insert(const std::pair<Key, Value> &pair) 
+    {
+        ull hs = Hash()(pair.first);
+        int hsmod = hs % mod;
+        for (int i = last[hsmod]; i; i = e[i].pre)
+            if (e[i].too == hs)
+                return 0;
+        
+        add(hsmod, hs, pair.second);
+        return 1;
     }
 
-    void clear() {
-        mapper.clear();
+    std::pair<Value, bool> erase(const Key &key) 
+    {
+        ull hs = Hash()(key);
+        int hsmod = hs % mod;
+        for (int i = last[hsmod], *j = &last[hsmod]; i; j = &e[i].pre, i = e[i].pre)
+            if (e[i].too == hs)
+            {
+                Value ans = e[i].val;
+                *j = e[i].pre;
+                size--;
+                return {ans, 1};
+            }
+
+        return {Value(), 0};
     }
 
-    Value *find(Key key) {
-        auto f = mapper.find(key);
-        if (f == mapper.end()) return nullptr;
-        return &(f->second);
+    bool empty() const 
+    {
+        return size == 0;
     }
 
-private:
-    std::unordered_map<Key, Value, Hash> mapper;
+    int get_size() const
+    {
+        return size;
+    }
+
+    void clear() 
+    {
+        tot = size = 0;
+        memset(last, 0, sizeof(int) * mod);
+    }
+
+    Value *find(Key key) 
+    {
+        ull hs = Hash()(key);
+        int hsmod = hs % mod;
+        for (int i = last[hsmod]; i; i = e[i].pre)
+            if (e[i].too == hs)
+                return &e[i].val;
+
+        return nullptr;
+    }
 };
 
 
@@ -457,12 +505,12 @@ struct HashString {
     }
 };
 
-//memo 关于写法：queue整万划分，queue基本在内存中进行，直到内存达万划入一个整万块。在refund时，一个个把整万块拿出来在栈空间检查，然后如果某个整万块退成了，把那个块在内存里改掉后在外存里全部覆写一遍。
+// //memo 关于写法：queue整万划分，queue基本在内存中进行，直到内存达万划入一个整万块。在refund时，一个个把整万块拿出来在栈空间检查，然后如果某个整万块退成了，把那个块在内存里改掉后在外存里全部覆写一遍。
 
-//memo 关于userorder的写法： 内存中维护一个{loginuser，the beginning of userorder-list} 的unordered_map,然后所有userorder-list 存入一个文件，如果某个user内存中有过长的order，就把它刷到文件末尾，并附加刷一个指向上一个块的位置。
-// 优化1.如果某次query_order发现碎片块过多了，就把它们全读入内存整合，然后在文件末尾写入一个整合后的连续链表。优化2. 维护某个外存整块100bit-200bit，200bit-300bit被用了多少字节。如果某一时刻发现其用了0字节，就知道它可用了。以此完成垃圾回收。
-//caution 因为毕竟是F操作，如果时耗足够小这两个优化是可以一起不用做的。如果队列够短，也是不用做整万划分的。
-//更正：写成vector
+// //memo 关于userorder的写法： 内存中维护一个{loginuser，the beginning of userorder-list} 的unordered_map,然后所有userorder-list 存入一个文件，如果某个user内存中有过长的order，就把它刷到文件末尾，并附加刷一个指向上一个块的位置。
+// // 优化1.如果某次query_order发现碎片块过多了，就把它们全读入内存整合，然后在文件末尾写入一个整合后的连续链表。优化2. 维护某个外存整块100bit-200bit，200bit-300bit被用了多少字节。如果某一时刻发现其用了0字节，就知道它可用了。以此完成垃圾回收。
+// //caution 因为毕竟是F操作，如果时耗足够小这两个优化是可以一起不用做的。如果队列够短，也是不用做整万划分的。
+// //更正：写成vector
 
-//bug 数据结构有bug，先把这个写好吧。
+// //bug 数据结构有bug，先把这个写好吧。
 #endif //MAIN_CPP_MAPS_H
